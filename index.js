8eab5acbe9a74d77797f3ea9f9087b7998604f70
Omotola <liadi.omotola@gmail.com>
2017-11-20 20:45:07 +0100

    setup database

--- index.js
+++ index.js
@@ -4,13 +4,17 @@ import morgan from 'morgan';
 
 
 const app = express();
+
 app.use(morgan('dev'));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+
 app.get('*', (req, res) => {
-  res.send('Welcome');
+  res.status(200).send({
+    message: 'Welcome',
+  });
 });
 
 
