e96a11c9a534f493005c8c78b022c3721ed9be07
Omotola <liadi.omotola@gmail.com>
2017-11-20 16:38:14 +0100

    setup express app

---
+++ index.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import morgan from 'morgan';
+
+
+const app = express();
+app.use(morgan('dev'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('*', (req, res) => {
+  res.send('Welcome');
+});
+
+
+app.listen(80, () => console.log('App running!'));
