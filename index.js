82464a576e7b4ddf8a2b1fe43a89fd725c59f072
Omotola <liadi.omotola@gmail.com>
2017-11-21 07:58:02 +0100

    setup routing

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+require('./server/routes')(app);
 
 app.get('*', (req, res) => {
   res.status(200).send({
