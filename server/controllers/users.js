d7c3c5ed35163cec0dd35e4e5008883d69c08e4a
Liadi <liadi.omotola@gmail.com>
2017-11-22 22:01:51 +0100

    validate unique email signup

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -2,33 +2,42 @@ const User = require('../models').User;
 import bcryptjs from 'bcryptjs';
 module.exports = {
 	createUser(req, res){
-        req.userPassword = bcryptjs.hashSync(req.userPassword, 10);
-        User.create({
-			userFirstName: req.userFirstName,
-            userLastName: req.userLastName,
-            userEmail: req.userEmail,
-            userPassword: req.userPassword,
-            userPhone: req.userPhone,
-            userStatus: req.userStatus,
-
-		}).then(user => {
-            return res.status(201).json({
-                message: 'User created',
-                status: true,
-                feed: {
-                    'firstname': req.userFirstName,
-                    'lastname': req.userLastName,
-                    'email': req.userEmail,
-                    'phone': req.userPhone,
-                    'user status': req.userStatus,
-                },
-            })
-        })
-        .catch(error => {
-            return res.status(400).json({
-                message: "pls fill in the fields appropriately",
-                status: false
-            })
+    req.userPassword = bcryptjs.hashSync(req.userPassword, 10);
+    User.findOne({ 
+      where: {userEmail: req.userEmail}
+    }).then((user) => {
+      if (user) {
+        return res.status(400).json({
+          message: 'account with email already exists',
+          status: false,
         });
+      }
+      User.create({
+        userFirstName: req.userFirstName,
+        userLastName: req.userLastName,
+        userEmail: req.userEmail,
+        userPassword: req.userPassword,
+        userPhone: req.userPhone,
+        userStatus: req.userStatus,
+      }).then(user => {
+        return res.status(201).json({
+          message: 'User created',
+          status: true,
+          feed: {
+            'firstname': req.userFirstName,
+            'lastname': req.userLastName,
+            'email': req.userEmail,
+            'phone': req.userPhone,
+            'user status': req.userStatus,
+          },
+        })
+      })
+      .catch(error => {
+        return res.status(400).json({
+          message: "pls fill in the fields appropriately",
+          status: false
+        })
+      });
+    });    
 	},
 }
