ebb9ae16779574c84dd620780bc14621d0fbf2ca
Liadi <liadi.omotola@gmail.com>
2017-12-03 13:56:41 +0100

    fix travis timeout

--- test/test.js
+++ test/test.js
@@ -15,4 +15,24 @@ describe('api', () => {
         done();
       });
   });
+
+  data = {
+    userFirstName: 'tola',
+    userLastName: 'liadi',
+    userEmail: 'liadi.omotola@gmail.com',
+    userPassword: 'fatBoy',
+    userPhoneNumber: '08181546011',
+  };
+  describe('user signup api', () => {
+    it('returns first name required', (done) => {
+      request.post('/api/v1/users/signup').send(data).end((err, res) => {
+        // expect(res.status).to.equal(201);
+        expect(res.body.message).to.equal('user created');
+        expect(res.body.status).to.equal(true);
+        done();
+      });
+    });
+  });
 });
+
+module.exports = app;
