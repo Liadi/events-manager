c90b8120546454c506e4e97b99c9b01a2e3a881a
Liadi <liadi.omotola@gmail.com>
2017-12-03 06:36:03 +0100

    user validation

--- server/middlewares/index.js
+++ server/middlewares/index.js
@@ -1,3 +1,8 @@
+import jwt from 'jsonwebtoken';
+
+process.env.SECRET_KEY = 'mysecretkey';
+const secret = process.env.SECRET_KEY;
+
 module.exports = {
   cleanData(req, res, next) {
     try {
@@ -35,4 +40,69 @@ module.exports = {
       });
     } next();
   },
+
+  validateToken(req, res, next) {
+    // ensure token found
+    req.token = req.body.token || req.headers.token;
+    if (!req.token) {
+      res.status(401).json({
+        message: 'You only have access, if you\'re logged in',
+        status: false,
+      });
+    }
+
+    // ensure same user
+    let verifiedJWT;
+    try {
+      verifiedJWT = jwt.verify(req.token, secret);
+    } catch (err) {
+      // token expired
+      return res.status(400).json({
+        message: 'pls, login',
+        status: false,
+      });
+    }
+    if (!verifiedJWT) {
+      return res.status(400).json({
+        message: 'Pls Login into your account or sign up',
+        satus: false,
+      });
+    }
+    req.userId = verifiedJWT.userId;
+    req.userType = verifiedJWT.userType;
+    next();
+  },
+
+  isAdmin(req, res, next) {
+    if (req.userType !== 'admin') {
+      return res.status(401).json({
+        message: 'access denied',
+        status: false,
+      });
+    }
+    next();
+  },
+
+  validateSignUpFields(req, res, next) {
+    if (!req.userPassword) {
+      return res.status(400).json({
+        message: 'Password required',
+        status: false,
+      });
+    }
+    if (!(req.userPassword.length >= 6)) {
+      return res.status(400).json({
+        message: 'password should have at least 6 characters',
+        status: false,
+      });
+    }
+    if (!req.userEmail) {
+      return res.status(400).json({
+        message: 'email required',
+        status: false,
+      });
+    }
+    next();
+  },
+
 };
