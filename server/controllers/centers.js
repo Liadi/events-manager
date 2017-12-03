016ae4a70cd5a77cec1a738eb89f0ee5846a7bc6
Liadi <liadi.omotola@gmail.com>
2017-12-03 06:33:32 +0100

    'create center' controller

--- server/controllers/centers.js
+++ server/controllers/centers.js
@@ -1,10 +1,34 @@
-const Center = require('./../models').Center;
-const User = require('./../models').User;
-import jwt from  'jsonwebtoken';
-process.env.SECRET_KEY = 'mysecretkey'; 
-const secret = process.env.SECRET_KEY; 
+import db from './../models';
+
+const { Center } = db;
 
 module.exports = {
-  
+  createCenter(req, res) {
+    Center.create({
+      centerName: req.centerName,
+      centerAddress: req.centerAddress,
+      centerState: req.centerState,
+      centerCity: req.centerCity,
+      centerCountry: req.centerCountry,
+      centerDescription: req.centerDescription,
+      centerMantra: req.centerMantra,
+      centerCapacity: req.centerCapacity,
+      centerRate: req.centerRate,
+      centerStatus: req.centerStatus,
+      centerAmenitites: req.centerAmenities,
 
+    }).then((center) => {
+      return res.status(201).json({
+        message: 'Center created',
+        center,
+        status: true,
+      });
+    }).catch((error) => {
+      const err = error.errors[0].message;
+      return res.status(400).json({
+        message: `${err}, pls fill in the fields appropriately`,
+        status: false,
+      });
+    });
+  },
 };
