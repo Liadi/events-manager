cc264c5fe19ebbc9125edbdb0b38ded9a45ed713
Liadi <liadi.omotola@gmail.com>
2018-01-02 02:37:12 +0100

    restructure

--- server/index.js
+++ server/index.js
@@ -1,15 +1,40 @@
 import express from 'express';
+import webpack from 'webpack';
+import webpackDevMiddleware from 'webpack-dev-middleware';
+import config from '../webpack.config.js';
 import bodyParser from 'body-parser';
 import morgan from 'morgan';
+import router from './routes'
+import path from 'path';
 
-
+const dist = path.join(__dirname, '');
 const app = express();
+const compiler = webpack(config);
+
+// Tell express to use the webpack-dev-middleware and use the webpack.config.js
+// configuration file as a base.
+if (app.get('env') !== 'test'){
+  app.use(webpackDevMiddleware(compiler, {
+  	publicPath: config.output.publicPath
+  }));
+}
 
 app.use(morgan('dev'));
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-require('./routes')(app);
+app.use('/api/v1/', router);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(dist, '../client/index.html'));
+});
+
+// other routes (404)
+app.all('*', (req, res) => {
+  res.status(404).json({
+    message: 'request not found, wrong url',
+    status: false,
+  });
+});
 
 module.exports = app;
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,40 +1,35 @@
+import express from 'express';
 import { centerControllers, eventControllers, userControllers } from '../controllers';
 import { centerMiddlewares, eventMiddlewares, userMiddlewares, cleanData } from '../middlewares/';
 import app from '../index';
 
-module.exports = (app) => {
-  app.get('/api/v1/', (req, res) => {
-    res.status(200).json({
-      message: 'Welcome to EM api',
-      status: true,
-    });
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.status(200).json({
+    message: 'Welcome to EM api',
+    status: true,
   });
+});
 
-  // users route
-  app.post('/api/v1/users/signup/', cleanData, userMiddlewares.validateUserFields, userControllers.createUser);
-  app.post('/api/v1/users/signin/', cleanData, userControllers.signIn);
-  // add admin
-  app.post('/api/v1/users/admin', cleanData, userMiddlewares.validateToken, userMiddlewares.isAdmin, userMiddlewares.validateUserFields, userControllers.createUser);
+// users route
+router.post('/users/signup/', cleanData, userMiddlewares.validateUserFields, userControllers.createUser);
+router.post('/users/signin/', cleanData, userControllers.signIn);
+// add admin
+router.post('/users/admin', cleanData, userMiddlewares.validateToken, userMiddlewares.isAdmin, userMiddlewares.validateUserFields, userControllers.createUser);
 
-  // centers route
-  app.post('/api/v1/centers/', cleanData, userMiddlewares.validateToken, userMiddlewares.isAdmin, centerControllers.createCenter);
-  app.put('/api/v1/centers/:centerId', cleanData, userMiddlewares.validateToken, userMiddlewares.isAdmin, centerControllers.modifyCenter);
-  app.get('/api/v1/centers/:centerId', cleanData, userMiddlewares.determineUser, centerControllers.getCenter);
-  app.delete('/api/v1/centers/:centerId', cleanData, userMiddlewares.validateToken, userMiddlewares.isAdmin, centerControllers.deleteCenter);
-  app.get('/api/v1/centers/', cleanData, userMiddlewares.validateToken, centerControllers.getAllCenters);
+// centers route
+router.post('/centers/', cleanData, userMiddlewares.validateToken, userMiddlewares.isAdmin, centerControllers.createCenter);
+router.put('/centers/:centerId', cleanData, userMiddlewares.validateToken, userMiddlewares.isAdmin, centerControllers.modifyCenter);
+router.get('/centers/:centerId', cleanData, userMiddlewares.determineUser, centerControllers.getCenter);
+router.delete('/centers/:centerId', cleanData, userMiddlewares.validateToken, userMiddlewares.isAdmin, centerControllers.deleteCenter);
+router.get('/centers/', cleanData, userMiddlewares.validateToken, centerControllers.getAllCenters);
 
-  // event route
-  app.post('/api/v1/events/', cleanData, userMiddlewares.validateToken, eventMiddlewares.validateEventFields, eventControllers.createEvent);
-  app.put('/api/v1/events/:eventId', cleanData, userMiddlewares.validateToken, eventMiddlewares.validateEventFields, eventControllers.modifyEvent);
-  app.get('/api/v1/events/:eventId', cleanData, userMiddlewares.validateToken, eventControllers.getEvent);
-  app.delete('/api/v1/events/:eventId', cleanData, userMiddlewares.validateToken, eventControllers.deleteEvent);
-  app.get('/api/v1/events/', cleanData, userMiddlewares.validateToken, eventControllers.getAllEvents);
+// event route
+router.post('/events/', cleanData, userMiddlewares.validateToken, eventMiddlewares.validateEventFields, eventControllers.createEvent);
+router.put('/events/:eventId', cleanData, userMiddlewares.validateToken, eventMiddlewares.validateEventFields, eventControllers.modifyEvent);
+router.get('/events/:eventId', cleanData, userMiddlewares.validateToken, eventControllers.getEvent);
+router.delete('/events/:eventId', cleanData, userMiddlewares.validateToken, eventControllers.deleteEvent);
+router.get('/events/', cleanData, userMiddlewares.validateToken, eventControllers.getAllEvents);
 
-  // other routes (404)
-  app.all('*', (req, res) => {
-    res.status(404).json({
-      message: 'request not found, wrong url',
-      status: false,
-    });
-  });
-};
+export default router;
