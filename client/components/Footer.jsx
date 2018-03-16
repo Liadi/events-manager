c07a98174e446fbaa255f60a9e5bafefa06692c2
Liadi <liadi.omotola@gmail.com>
2018-03-16 12:24:16 +0100

    footer styling

--- client/components/Footer.jsx
+++ client/components/Footer.jsx
@@ -5,47 +5,42 @@ import '../style/index.scss';
 const Footer = () => {
   return (
     <footer className="container-fluid">
-      <div className='row'>
-        <div className='col-lg-6'>
-          <div className='d-flex d-flex-row justify-content-between'>
-            <Link className='link' to='/under-construction'>
-              <i className='fa fa-facebook-square fa-4x'></i>
-            </Link>
-            <Link className='link' to='/under-construction'>
-              <i className='fa fa-twitter-square fa-4x'></i>
-            </Link>
-            <Link className='link' to='/under-construction'>
-              <i className='fa fa-instagram fa-4x'></i>
-            </Link>
-            <Link className='link' to='/under-construction'>
-              <i className='fa fa-whatsapp fa-4x'></i>
-            </Link>
-          </div>
+      <div className='row space-bottom-2x space-top-2x'>
+        <div className='text-center col-lg-4 space-top'>
+          <h2 className='footer-link'> (+234)8181546011 </h2>
+          <h2 className='footer-link'> (+234)9038657766 </h2>
         </div>
 
-        <div className='col-lg-6'>
-          <div className='d-flex flex-row justify-content-between'>
-            <div className='space-top-2x'>
-              <p><Link className='link' to='/under-construction'>About Us</Link></p>
-              <p><Link className='link' to='/under-construction'>Latest News</Link></p>
-              <p><Link className='link' to='/under-construction'>Jobs</Link></p>
-              <p><Link className='link' to='/under-construction'>Contributors</Link></p>
-            </div>
-            <div className= 'space-top-2x'>
-              <p>P.O.B (12024 Ode Remo, Ogun) </p>
-              <p>Fax: 738893:)</p>
-              <p>EM@gmail.com </p>
-              <p>(+234)818 154 6011</p>
-            </div>
-          </div>
+        <div className='text-center col-lg-4 space-top'>
+          <h2> <Link to='/under-construction' className='link footer-link'> Contact Info </Link></h2>
         </div>
 
-        <div className='footer-bottom space-top-2x'>
-          <p>Labule Inititaive (10 Imobido street by Oxford Computer Institute, Iperu-Remo, Ogun)</p>
-          <p>Copyight {new Date().getFullYear()}</p>
+        <div className='text-center col-lg-4 space-top'>
+          <h2> <a href='/www.github.com/liadi/events-manager' className='link footer-link'> Project Info </a> </h2>
         </div>
-
       </div>
+
+      <div className="dropdown-divider"></div>
+
+      <div className='row'>
+        <div className='col-lg-4 col-md-6 col-sm-8'>
+          <Link className='link footer-link space-right' to='/under-construction'>
+            <i className='fa fa-facebook-square fa-4x'></i>
+          </Link>
+          <Link className='link footer-link space-right' to='/under-construction'>
+            <i className='fa fa-twitter-square fa-4x'></i>
+          </Link>
+          <Link className='link footer-link space-right' to='/under-construction'>
+            <i className='fa fa-instagram fa-4x'></i>
+          </Link>
+          <Link className='link footer-link space-right' to='/under-construction'>
+            <i className='fa fa-whatsapp fa-4x'></i>
+          </Link>
+        </div>
+      </div>
+
+      <div className='space-top'><small>liadi.omotola@gmail.com</small></div>
+      <div><small>Copyright 2018, Labule Initiative</small></div>
     </footer>
   )
 }
